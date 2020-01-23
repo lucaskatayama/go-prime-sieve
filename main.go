caee83f693b97951b625a4b19fc68d0d0e1a561b
Lucas Katayama <lucaskatayama@gmail.com>
2020-01-23 12:29:07 -0300

    back code

--- main.go
+++ main.go
@@ -1,12 +1,12 @@
 package main
 
-func Generate(ch chan<- int) {
+func Generate(ch chan int) {
 	for i := 2; ; i++ {
 		ch <- i
 	}
 }
 
-func Filter(src <-chan int, dst chan<- int, prime int) {
+func Filter(src chan int, dst chan int, prime int) {
 	for i := range src {
 		if i%prime != 0 {
 			dst <- i
